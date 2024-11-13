6c75b009fb2b5105923fc5507d8fd10d825d25a2
7ussainnabeel <hnabeel3@gmail.com>
2024-11-13 12:43:00 +0300

    chnages

---
+++ js/script.js
@@ -0,0 +1,5 @@
+// script.js
+document.addEventListener("DOMContentLoaded", function () {
+    // JavaScript code can be added here if needed in the future.
+    console.log("JavaScript is linked successfully!");
+});
