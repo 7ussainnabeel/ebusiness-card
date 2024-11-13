52f687b7a3ad846382eb21905136c55fd2fd5fa4
7ussainnabeel <hnabeel3@gmail.com>
2024-11-13 23:55:32 +0300

    Final Changes - Operational Business Card

--- js/script.js
+++ js/script.js
@@ -3,3 +3,10 @@ document.addEventListener("DOMContentLoaded", function () {
     // JavaScript code can be added here if needed in the future.
     console.log("JavaScript is linked successfully!");
 });
+// Instagram Embed Script
+(function() {
+    var s = document.createElement('script');
+    s.async = true;
+    s.src = "//www.instagram.com/embed.js";
+    document.body.appendChild(s);
+})();
